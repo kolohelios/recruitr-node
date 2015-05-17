e1ed55b148a0359ccab2c3913b3eff2efe0a8cac
Chris Oyler <christopher.oyler@gmail.com>
2015-05-17 11:10:26 -0700

    profile.show fully functional? and tested to 100% coverage

--- test/plugins/endpoints/profiles/show.js
+++ test/plugins/endpoints/profiles/show.js
@@ -12,11 +12,13 @@ var Sinon = require('sinon');
 var lab = exports.lab = Lab.script();
 var describe = lab.experiment;
 var expect = Chai.expect;
+var assert = Chai.assert;
 var it = lab.test;
 var before = lab.before;
 var after = lab.after;
 var beforeEach = lab.beforeEach;
 var Profile = require('../../../../lib/models/profile');
+var User = require('../../../../lib/models/user');
 
 var server;
 
@@ -46,8 +48,34 @@ describe('GET /profiles/{profileId?}', function(){
       done();
     });
   });
-  it('should throw an error', function(done){
+  it('should error on non-existent profile', function(done){
+    server.inject({method: 'GET', url: '/profiles/c00110000980005342000017', credentials: {_id: 'b00000000000000000000001'}}, function(response){
+      expect(response.statusCode).to.equal(400);
+      done();
+    });
+  });
+  it('should get one specific profile and restricted info', function(done){
+    server.inject({method: 'GET', url: '/profiles/a00000000000000000000002', credentials: {_id: 'b00000000000000000000001'}}, function(response){
+      expect(response.statusCode).to.equal(200);
+      expect(response.result._id.toString()).to.equal('a00000000000000000000002');
+      expect(response.result.bio).to.not.be.ok;
+      expect(response.result.photo).to.not.be.ok;
+      expect(response.result.lastName).to.not.be.ok;
+      assert.notOk(response.result.contact[0]);
+      assert.notOk(response.result.social[0]);
+      done();
+    });
+  });
+  it('should throw an error on Profile.findOne', function(done){
     var stub = Sinon.stub(Profile, 'findOne').yields(new Error());
+    server.inject({method: 'GET', url: '/profiles/a00000000000000000000002', credentials: {_id: 'b00000000000000000000004'}}, function(response){
+      expect(response.statusCode).to.equal(400);
+      stub.restore();
+      done();
+    });
+  });
+  it('should throw an error on Profile.findById', function(done){
+    var stub = Sinon.stub(User, 'findById').yields(new Error());
     server.inject({method: 'GET', url: '/profiles/a00000000000000000000002', credentials: {_id: 'b00000000000000000000004'}}, function(response){
       expect(response.statusCode).to.equal(400);
       stub.restore();
