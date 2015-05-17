2d19b110d52936572e3876a18e6e9d93dc76cc5e
Chris Oyler <christopher.oyler@gmail.com>
2015-05-16 17:13:14 -0700

    edit profile fully tested and working

--- test/plugins/endpoints/profiles/show.js
+++ test/plugins/endpoints/profiles/show.js
@@ -34,7 +34,6 @@ describe('GET /profiles/{profileId?}', function(){
       done();
     });
   });
-  
   after(function(done){
     server.stop(function(){
       Mongoose.disconnect(done);
@@ -44,27 +43,6 @@ describe('GET /profiles/{profileId?}', function(){
     server.inject({method: 'GET', url: '/profiles/a00000000000000000000002', credentials: {_id: 'b00000000000000000000004'}}, function(response){
       expect(response.statusCode).to.equal(200);
       expect(response.result._id.toString()).to.equal('a00000000000000000000002');
-      // expect(response.result[0].firstName).to.equal('Evan');
-      // expect(response.result[0].photo).to.equal('http://www.yahoo.com');
-      // expect(response.result[0].lastName).to.equal('Gatchell');
-      // expect(response.result[0].createdAt).to.be.ok;
-      // expect(response.result[0].skills).to.have.length(3);
-      // expect(response.result[0].exposure).to.have.length(3);
-      // expect(response.result[0].bio).to.equal('Developer B');
-      // expect(response.result[0].location).to.equal('Fremont, CA');
-      // expect(response.result[0].interests).to.have.length(3);
-      // expect(response.result[0].remote).to.equal(true);
-      // expect(response.result[0].relocate).to.equal(true);
-      // expect(response.result[0].locationPref).to.have.length(3);
-      // expect(response.result[0].education).to.have.length(3);
-      // expect(response.result[0].contact.email).to.equal('test2@test.com');
-      // expect(response.result[0].contact.phone).to.equal('510-345-1234');
-      // expect(response.result[0].contact.address).to.equal('345 Main St, Fremont, CA 94555');
-      // expect(response.result[0].social.github).to.equal('https://github.com/greygatch');
-      // expect(response.result[0].social.facebook).to.equal('BookFace3');
-      // expect(response.result[0].social.twitter).to.equal('https://twitter.com/greygatch');
-      // expect(response.result[0].social.linkedIn).to.equal('https://www.linkedin.com/in/greygatch');
-      // expect(response.result[0].social.stackoverflow).to.equal('http://stackoverflow.com/users/4750158/greygatch');
       done();
     });
   });
@@ -76,6 +54,4 @@ describe('GET /profiles/{profileId?}', function(){
       done();
     });
   });
-  
-  
 });
